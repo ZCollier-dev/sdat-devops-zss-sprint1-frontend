e7d77b4488c3e68afc2d0061658139648066964d
Zack <zacharycollier36@protonmail.com>
2025-06-24 20:49:36 -0230

    Added getFromAPI method to main class, added example API call to main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
 
+public class Main {
+    public static void getFromAPI(String url, HttpClient client){
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response ->\n" + response.body());
+        } catch (IOException e) {
+            System.out.println("Runtime");
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Base URL
+        String baseURL = "http://localhost:8080/api";
+        HttpClient client = HttpClient.newBuilder().build();
+
+        String modifiedURL = baseURL + "/airports/by-city";
+
+        getFromAPI(modifiedURL, client);
     }
 }
